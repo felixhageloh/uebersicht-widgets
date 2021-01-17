d114524c87f90ab0f2281913da5bf29f2deb67b6
Felix Hageloh <felix.hageloh@gmail.com>
2021-01-17 10:59:36 +0100

    Apply fix for main repo to browser as well
    The browser github API did not return the status code, so the fix did
    not work

--- src/GitHubBrowserApi.js
+++ src/GitHubBrowserApi.js
@@ -1,4 +1,4 @@
-var baseUrl = 'https://api.github.com';
+var baseUrl = "https://api.github.com";
 var currentCallback;
 var busy = false;
 
@@ -9,17 +9,16 @@ window.githubApiCallback = function githubApiCallback(json) {
   }
   json.meta.status < 300
     ? currentCallback(null, json.data)
-    : currentCallback(json.data)
-    ;
+    : currentCallback({ ...json.data, status: json.meta.status });
 };
 
 exports.getJSON = function getJSON(path, callback) {
   if (busy) {
-    throw new Error('API request already in progress.');
+    throw new Error("API request already in progress.");
   }
   busy = true;
-  var script = document.createElement('SCRIPT');
-  script.src = baseUrl + '/' + path + '?callback=githubApiCallback';
+  var script = document.createElement("SCRIPT");
+  script.src = baseUrl + "/" + path + "?callback=githubApiCallback";
   document.body.appendChild(script);
   currentCallback = callback;
 };
